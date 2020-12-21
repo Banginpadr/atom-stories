fa9a7d75057ca6a0e40fe84ed997227722b8b893
boss <3503307+Banginpadr@users.noreply.github.com>
2020-12-22 00:54:16 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const _prod_ = process.env.NODE_ENV === "production";
 export const accessTokenKey = "@stories/token" + (_prod_ ? "" : "dev");
 export const refreshTokenKey = "@stories/refresh-token" + (_prod_ ? "" : "dev");
 export const apiBaseUrl = _prod_
-  ? "https://atom-stories-295306.uk.r.appspot.com"
-  : "http://localhost:8080";
+  ? "https://hunty.atwebpages.com"
+  : "http://127.0.0.1:8080";
